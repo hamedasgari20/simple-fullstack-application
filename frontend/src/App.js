d65140806ef4244c58cadc145e7f59423858c606
Hamed Asgari <hamedasgari20@yahoo.com>
2022-05-14 13:23:58 +0430

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+import AddMovie from './AddMovie'
+
+function App() {
+   
+  return (
+    <div className="App">
+      <AddMovie/>
+      
+    </div>
+  );
+}
+
+export default App;
